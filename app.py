3447a2bd87a7237b96dceb0df01361b3dad209e2
apiena-02 <apiena1@gmail.com>
2024-07-23 16:54:12 -0400

    Accessing Downloads Folder

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 13:10:24 2024
+
+@author: apienaselvarajah
+"""
+
+import os
+
+entries = os.listdir('/Users/apienaselvarajah/Downloads')
+
+for entry in entries:
+    print(entry)
