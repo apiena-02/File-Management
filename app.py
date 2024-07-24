eaef51245ac0772aa12a1195d35680a0340bb81d
apiena-02 <apiena1@gmail.com>
2024-07-24 13:34:21 -0400

    Imported watchdog

--- app.py
+++ app.py
@@ -7,8 +7,33 @@ Created on Tue Jul 23 13:10:24 2024
 """
 
 import os
+import sys
+import logging
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
-entries = os.listdir('/Users/apienaselvarajah/Downloads')
+source = "/Users/apienaselvarajah/Downloads"
 
-for entry in entries:
-    print(entry)
+class manageDownloads():
+    entries = os.listdir(source)
+
+    for entry in entries:
+        print(entry)
+    
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    path = source
+    event_handler = manageDownloads()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while observer.is_alive():
+            observer.join(1)
+    finally:
+        observer.stop()
+        observer.join()
